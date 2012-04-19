b459ed338603c7096c381c739c0041b500a72719
Lindsey Hanna <lindsey.hanna@msu.montana.edu>
2012-04-19 10:47:00 -0600

    cloned

---
+++ TankGame/TankGame/GameView.m
@@ -0,0 +1,31 @@
+//
+//  GameView.m
+//  TankGame
+//
+//  Created by default on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameView.h"
+
+@implementation GameView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
