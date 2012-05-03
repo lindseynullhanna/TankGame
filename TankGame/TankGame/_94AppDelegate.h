988cdf05c0c7c23605cbcae4aaa8f2149c64f778
John Bender <john@generalui.com>
2012-05-03 16:11:14 -0700

    grading

--- TankGame/TankGame/_94AppDelegate.h
+++ TankGame/TankGame/_94AppDelegate.h
@@ -15,3 +15,15 @@
 @property BOOL useAI;
 
 @end
+
+/*
+ Good job - you made it! You got all the requirements done and created an app
+ that's fun to play. The only glitch I noticed is that the AI still tries to aim
+ the cannon when it's in two-player mode, leading to a battle over the direction
+ of player 2's fire. But that's a pretty small thing. You should be proud of
+ finishing it, and vow hereafter not to discount how much work goes into mobile
+ software development.
+ 
+ Grade: 100%
+ Course grade: 98% (A)
+*/
